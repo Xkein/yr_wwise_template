73cdb91e6cab3207a7a6046710797a77da9b04db
Xkein <739502808@qq.com>
2024-08-03 01:00:08 +0800

    init commit

---
+++ GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,63 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BULLET_DETONATE = 666185888U;
+    } // namespace EVENTS
+
+    namespace SWITCHES
+    {
+        namespace SURFACE
+        {
+            static const AkUniqueID GROUP = 1834394558U;
+
+            namespace SWITCH
+            {
+                static const AkUniqueID BEACH = 4075332698U;
+                static const AkUniqueID CLEAR = 1754255536U;
+                static const AkUniqueID ICE = 344481046U;
+                static const AkUniqueID RAILROAD = 2722214071U;
+                static const AkUniqueID ROAD = 2110808655U;
+                static const AkUniqueID ROCK = 2144363834U;
+                static const AkUniqueID ROUGH = 1262240040U;
+                static const AkUniqueID TIBERIUM = 3960883458U;
+                static const AkUniqueID TUNNEL = 3059984139U;
+                static const AkUniqueID WALL = 2108779961U;
+                static const AkUniqueID WATER = 2654748154U;
+                static const AkUniqueID WEEDS = 588027413U;
+            } // namespace SWITCH
+        } // namespace SURFACE
+
+    } // namespace SWITCHES
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID ENGINEUSED = 2066667852U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+    } // namespace BUSSES
+
+    namespace AUDIO_DEVICES
+    {
+        static const AkUniqueID NO_OUTPUT = 2317455096U;
+        static const AkUniqueID SYSTEM = 3859886410U;
+    } // namespace AUDIO_DEVICES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
