cabcc085d9388ab3e22df162e7f567e2419c7392
Xkein <739502808@qq.com>
2024-08-04 22:52:20 +0800

    add master mixer and game syncs

--- GeneratedSoundBanks/Wwise_IDs.h
+++ GeneratedSoundBanks/Wwise_IDs.h
@@ -13,8 +13,32 @@ namespace AK
 {
     namespace EVENTS
     {
-        static const AkUniqueID BULLET_DETONATE = 666185888U;
+        static const AkUniqueID M_GAMEOVER = 155272345U;
+        static const AkUniqueID M_INVASION = 4033210584U;
+        static const AkUniqueID M_NONE = 443355295U;
+        static const AkUniqueID M_NORMAL = 2309605244U;
+        static const AkUniqueID M_UNDERATTACK = 2821901421U;
+        static const AkUniqueID M_WINNING = 3431848355U;
     } // namespace EVENTS
+
+    namespace STATES
+    {
+        namespace MUSIC
+        {
+            static const AkUniqueID GROUP = 3991942870U;
+
+            namespace STATE
+            {
+                static const AkUniqueID GAMEOVER = 4158285989U;
+                static const AkUniqueID INVASION = 1335043396U;
+                static const AkUniqueID NONE = 748895195U;
+                static const AkUniqueID NORMAL = 1160234136U;
+                static const AkUniqueID UNDERATTACK = 2862835585U;
+                static const AkUniqueID WINNING = 2996998095U;
+            } // namespace STATE
+        } // namespace MUSIC
+
+    } // namespace STATES
 
     namespace SWITCHES
     {
@@ -41,6 +65,11 @@ namespace AK
 
     } // namespace SWITCHES
 
+    namespace GAME_PARAMETERS
+    {
+        static const AkUniqueID OBJECTHP = 1335699628U;
+    } // namespace GAME_PARAMETERS
+
     namespace BANKS
     {
         static const AkUniqueID INIT = 1355168291U;
@@ -49,7 +78,14 @@ namespace AK
 
     namespace BUSSES
     {
+        static const AkUniqueID _3D_BUS = 2337546123U;
+        static const AkUniqueID ENVIRONMENT = 1229948536U;
+        static const AkUniqueID EVA = 932389257U;
         static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MUSIC = 3991942870U;
+        static const AkUniqueID SOUNDS = 1492361653U;
+        static const AkUniqueID UNIT = 1304109583U;
+        static const AkUniqueID VOICE = 3170124113U;
     } // namespace BUSSES
 
     namespace AUDIO_DEVICES
